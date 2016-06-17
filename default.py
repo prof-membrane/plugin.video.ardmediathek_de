78894979ad442045a154168db76a2cad2b08c492
prof-membrane <pr0lckr@googlemail.com>
2016-06-17 15:44:20 +0200

    initial commit

---
+++ default.py
@@ -0,0 +1,7 @@
+﻿# -*- coding: utf-8 -*-
+import urllib,urllib2,re,random,xbmcplugin,xbmcgui,xbmcaddon,cookielib,HTMLParser,datetime,os
+from time import gmtime, strftime
+
+import libArd
+
+libArd.list()
