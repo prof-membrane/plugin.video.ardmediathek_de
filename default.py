245385cad17ad7c03b2e7c6415426a823346278a
prof-membrane <pr0lckr@googlemail.com>
2017-09-04 22:06:25 +0200

    brought add-on up to date

--- default.py
+++ default.py
@@ -1,7 +1,4 @@
 ﻿# -*- coding: utf-8 -*-
-import urllib,urllib2,re,random,xbmcplugin,xbmcgui,xbmcaddon,cookielib,HTMLParser,datetime,os
-from time import gmtime, strftime
+import libard
 
-import libArd
-
-libArd.list()
+libard.list()
